7ecd9ec5695e6d743756c25b1237d895341f9028
Oleg Khavroshin <oleg_khavroshin@epam.com>
2022-04-03 12:22:13 +0400

    Async backend

---
+++ DiplomaApplication/back-end.py
@@ -0,0 +1,101 @@
+"""
+1. Retrieve a portion of data from API (Variant 7) and store it in a database
+2. Update data on demand
+3. Update DB schema if needed on app’s update
+
+Variant 7. Using API https://www.metaweather.com/api/ get data about weather in Moscow for current month
+and store it into your DB
+(id, weather_state_name, wind_direction_compass, created, applicable_date, min_temp, max_temp, the_temp).
+Output the data by date (the date is set) in form of a table and sort them by created in ascending order.
+"""
+import calendar
+import sys
+from calendar import monthrange
+import datetime
+from db import Database
+import asyncio
+import aiohttp
+
+
+URL_API = 'https://www.metaweather.com/api/location/2122265'  # Moscow
+attrs = {
+    'id': 'bigint',
+    'weather_state_name': 'varchar',
+    'wind_direction_compass': 'varchar',
+    'created': 'timestamp',
+    'applicable_date': 'date',
+    'min_temp': 'float',
+    'max_temp': 'float',
+    'the_temp': 'float'
+}
+db = Database()
+
+
+class AsyncWeatherCall(object):
+    def __init__(self, urls, ids):
+        self.stri = []
+        self.urls = urls
+        self.ids = ids
+
+    async def call_url(self, url):
+        session = aiohttp.ClientSession()
+        response = await session.get(url)
+        data = await response.json()
+        result = ',\n'.join('(' + ','.join(f"'{(item[index])}'" for index in attrs.keys()) + ')' for item in data
+                            if item['id'] not in self.ids)
+        if result:
+            self.stri.append(result)
+        await session.close()
+
+    def do_async(self):
+        futures = [self.call_url(url) for url in self.urls]
+        loop = asyncio.new_event_loop()
+        loop.run_until_complete(asyncio.wait(futures))
+        return ',\n'.join(self.stri)
+
+
+def recreate_tables():
+    sql_cmd = f'''
+        CREATE table if not exists public.weather({', '.join(f'{key} {value} NULL' for key, value in attrs.items())});
+    '''
+    db.execute(sql_cmd)
+
+
+def get_ids(dt):
+    sql_cmd = f"""select id from public.weather
+    where applicable_date between
+        '{dt.strftime('%Y-%m')}-01' and '{dt.strftime('%Y-%m')}-{monthrange(dt.year, dt.month)[1]}'
+    """
+    ids = db.select(sql_cmd)
+    return [i[0] for i in ids]
+
+
+def backend(year, month, day):
+    year = int(year)
+    month = int(month)
+    day = int(day)
+    recreate_tables()
+    gen_ids = get_ids(datetime.date(year, month, day))
+    print(gen_ids)
+    urls = [f'{URL_API}/{year}/{month}/{cur_day + 1}' for cur_day in range(calendar.monthrange(year, month)[1])]
+    day_val = AsyncWeatherCall(urls, gen_ids).do_async()
+    sql_cmd = ''
+    print(day_val)
+    if day_val:
+        sql_cmd += f'''
+        insert into public.weather({','.join(f'"{index}"' for index in attrs.keys())}) values {day_val};
+        '''
+    if sql_cmd:
+        db.execute(sql_cmd)
+    db.conn.close()
+
+
+def usage():
+    print(f'usage: {sys.argv[0]} year month day')
+
+
+if __name__ == '__main__':
+    try:
+        backend(*sys.argv[1:4])
+    except TypeError as e:
+        usage()
---
+++ DiplomaApplication/db.py
@@ -0,0 +1,57 @@
+import psycopg2
+
+DB = "devops_school"
+DB_USER = "oleg"
+DB_PASSWORD = "oleg"
+DB_HOST = "db"
+DB_PORT = "5432"
+
+
+class Database:
+    """PostgreSQL Database class."""
+
+    def __init__(
+            self
+            # ,DB_HOST,
+            # DB_USER,
+            # DB_PASSWORD,
+            # DB_PORT,
+            # DB
+    ):
+        self.host = DB_HOST
+        self.username = DB_USER
+        self.password = DB_PASSWORD
+        self.port = DB_PORT
+        self.dbname = DB
+        self.conn = None
+
+    def connect(self):
+        """Connect to a Postgres database."""
+        if self.conn is None:
+            try:
+                self.conn = psycopg2.connect(
+                    host=self.host,
+                    user=self.username,
+                    password=self.password,
+                    port=self.port,
+                    dbname=self.dbname
+                )
+            except psycopg2.DatabaseError as e:
+                raise e
+
+    def select(self, query):
+        """Run a SQL query to select rows from table."""
+        self.connect()
+        with self.conn.cursor() as cur:
+            cur.execute(query)
+            records = [row for row in cur.fetchall()]
+            cur.close()
+            return records
+
+    def execute(self, query):
+        """Run a SQL query with no result - insert or update or delete etc"""
+        self.connect()
+        with self.conn.cursor() as cur:
+            cur.execute(query)
+            self.conn.commit()
+            cur.close()
