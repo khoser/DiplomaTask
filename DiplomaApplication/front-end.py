74a45d755ff6d634654f5b5c5e217248cbee6165
Oleg Khavroshin <oleg_khavroshin@epam.com>
2022-04-04 12:05:42 +0400

    Add frontend

---
+++ DiplomaApplication/front-end.py
@@ -0,0 +1,66 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from db import Database
+from datetime import date as dt
+
+db = Database()
+app = FastAPI()
+
+
+def prepare_table(rec, date):
+    html = f"""
+    <html>
+    <head><title>Devops school - weather in Moscow</title></head>
+    <body>
+    <center><b>Devops school - weather in Moscow at {date}</b></center>
+    <hr>
+        <table align="center" valign="center" border="1" cellspacing="0" cellpadding="5">
+            <tr>
+                <!--td><b>applicable_date</b></td-->
+                <td><b>created</b></td>
+                <td><b>weather_state_name</b></td>
+                <td><b>wind_direction_compass</b></td>
+                <td><b>min_temp</b></td>
+                <td><b>max_temp</b></td>
+                <td><b>the_temp</b></td>
+            </tr>
+    """
+    for row in rec:
+        html += "<tr>\n"
+        for col in row:
+            html = html + f"<td>{col}</td>\n"
+        html += "</tr>\n"
+    html += "</table></body></html>"
+    return html
+
+
+@app.get("/", response_class=HTMLResponse)
+async def main():
+    sql_cmd = f"""select
+        -- applicable_date,
+        created,
+        weather_state_name,
+        wind_direction_compass,
+        min_temp,
+        max_temp,
+        the_temp
+    from public.weather
+    where applicable_date = '{dt.today()}'
+    order by created asc;"""
+    return prepare_table(db.select(sql_cmd), dt.today())
+
+
+@app.get("/{date}", response_class=HTMLResponse)
+async def get_by_date(date):
+    sql_cmd = f"""select
+       -- applicable_date,
+        created,
+        weather_state_name,
+        wind_direction_compass,
+        min_temp,
+        max_temp,
+        the_temp 
+    from public.weather
+    where applicable_date = '{date}'
+    order by created asc;"""
+    return prepare_table(db.select(sql_cmd), date)
